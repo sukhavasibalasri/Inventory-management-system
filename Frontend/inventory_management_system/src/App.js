1f4b1675caa01c2d112cbc2b81d4e15e8fdf1690
Sukhavasi Bala Sri <sukhavasibalasri@gmail.com>
2026-09-18 15:09:26 +0530

    Redirect to login after logout

--- Frontend/inventory_management_system/src/App.js
+++ Frontend/inventory_management_system/src/App.js
@@ -16,6 +16,10 @@ import {
   Navigate
 } from 'react-router-dom';
 
+function ProtectedRoute({ user, children }) {
+  return user ? children : <Navigate to="/login" replace />;
+}
+
 
 
 
@@ -35,9 +39,9 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={user ? <Home /> : <Navigate to="/login" replace />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/insertproduct" element={<InsertProduct />} />
-          <Route path="/updateproduct/:id" element={<UpdateProduct />} />
+          <Route path="/products" element={<ProtectedRoute user={user}><Products /></ProtectedRoute>} />
+          <Route path="/insertproduct" element={<ProtectedRoute user={user}><InsertProduct /></ProtectedRoute>} />
+          <Route path="/updateproduct/:id" element={<ProtectedRoute user={user}><UpdateProduct /></ProtectedRoute>} />
           <Route path="/about" element={<About />} />
           <Route path="/login" element={user ? <Navigate to="/" replace /> : <Login onLogin={setUser} />} />
           <Route path="/register" element={user ? <Navigate to="/" replace /> : <Register onLogin={setUser} />} />
