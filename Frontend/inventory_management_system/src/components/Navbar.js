4379becac50f0e74ada4a15e28c9d172e15bc259
Sukhavasi Bala Sri <sukhavasibalasri@gmail.com>
2026-09-18 15:17:55 +0530

    Align navbar search and profile

--- Frontend/inventory_management_system/src/components/Navbar.js
+++ Frontend/inventory_management_system/src/components/Navbar.js
@@ -9,23 +9,25 @@ export default function Navbar(props) {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav navbar_left mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active text-white fs-4" aria-current="page" href="/">{props.title}</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link active text-white fs-4" aria-current="page" href="/about">About</a>
               </li>
-              {props.user && <li className="nav-item profile_item">
-                <span className="profile_symbol" aria-hidden="true">{props.user.name.charAt(0).toUpperCase()}</span>
-                <span className="profile_name">{props.user.name}</span>
-              </li>}
-              {props.user && <li className="nav-item"><button className="nav-link active text-white fs-4 nav_logout" onClick={props.onLogout}>Log out</button></li>}
             </ul>
-            <form className="d-flex" role="search">
+            <form className="d-flex navbar_search" role="search">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
               <button className="btn btn-primary fs-5" type="submit">Search</button>
             </form>
+            <div className="navbar_right">
+              {props.user && <div className="profile_item">
+                <span className="profile_symbol" aria-hidden="true">{props.user.name.charAt(0).toUpperCase()}</span>
+                <span className="profile_name">{props.user.name}</span>
+              </div>}
+              {props.user && <button className="nav-link active text-white fs-4 nav_logout" onClick={props.onLogout}>Log out</button>}
+            </div>
           </div>
         </div>
 
